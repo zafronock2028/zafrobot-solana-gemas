520a18144aa6f937149b77987f6f089041228848
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-05-15 20:48:56 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # main.py
+import os
 import threading
 import time
-import os
 from flask import Flask
 from dexscreener import scan_tokens
 from db import create_table, insert_token
-from telegram_bot import telegram_bot  # Este ya tiene el método .run()
+from telegram_bot import telegram_bot
 
 app = Flask(__name__)
 
@@ -26,13 +26,8 @@ def scanner_loop():
         time.sleep(30)
 
 if __name__ == '__main__':
-    # Iniciar hilo solo para el escáner
     threading.Thread(target=scanner_loop, daemon=True).start()
+    threading.Thread(target=telegram_bot.run, daemon=True).start()
 
-    # Iniciar el bot en el hilo principal
-    telegram_bot.run()
-
-    # Servidor web (solo útil si quieres ver la web)
-    # Puedes omitir si solo usas Telegram
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
