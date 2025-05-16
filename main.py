a4e85803a6411cd65de9ce163f333ba58b52376d
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-05-15 22:14:24 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from telegram_bot import start_bot
 from pump_scanner import start_scanner
+from db import init_db  # ← Esto es clave
+
 import threading
 
 if __name__ == '__main__':
+    init_db()  # ← Esto crea automáticamente tokens.db
     threading.Thread(target=start_bot).start()
     start_scanner()
